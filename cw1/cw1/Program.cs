0c67fc3f6d2f339e8875207502357f6282cd487a
Robert Raczkowski <s17794@pjwstk.edu.pl>
2020-10-24 15:09:59 +0200

    Pierwszy commit

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-zA-Z0-9]+@+[a-z.]+");
+             
+                MatchCollection matches = regex.Matches(html);
+                foreach (var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
